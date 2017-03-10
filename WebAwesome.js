25d88a51ef2188ea4ce6d0bfc6f397520ddeb2f6
Siviwe Xakaza <xakazampendulo@gmail.com>
2017-03-10 09:53:10 +0200

    Add files via upload
    First increment

---
+++ WebAwesome.js
@@ -0,0 +1,67 @@
+﻿
+$(function () {
+
+    //Text inputs
+
+    $('.web-form').blur(function () {
+
+        if ($(this).val() =='') {
+            $(this).removeClass('web-form');
+            $(this).removeClass('web-form-success');
+            $(this).addClass('web-form-error');
+        }
+
+    });
+
+    $('.web-form').keydown(function () {
+
+        if ($(this).val() != '') {
+            $(this).removeClass('web-form-error');
+            $(this).addClass('web-form-success');
+        }
+
+    });
+
+
+
+    $('.web-form-error').blur(function () {
+
+        if ($(this).val() == '') {
+            $(this).removeClass('web-form');
+            $(this).removeClass('web-form-success');
+            $(this).addClass('web-form-error');
+        }
+
+    });
+
+    $('.web-form-error').keydown(function () {
+
+        if ($(this).val() != '') {
+            $(this).removeClass('web-form-error');
+            $(this).addClass('web-form-success');
+        }
+
+    });
+
+
+
+    $('.web-form-success').blur(function () {
+
+        if ($(this).val() == '') {
+            $(this).removeClass('web-form');
+            $(this).removeClass('web-form-success');
+            $(this).addClass('web-form-error');
+        }
+
+    });
+
+    $('.web-form-success').keydown(function () {
+
+        if ($(this).val() != '') {
+            $(this).removeClass('web-form-error');
+            $(this).addClass('web-form-success');
+        }
+
+    });
+
+});
